14d7d83d0cb316188fa814d31a0401ec477e66ce
Tulio Vieira <vieira.tulio@gmail.com>
2021-01-03 18:24:16 -0300

    fixed issue with banner

---
+++ build/precache-manifest.98a6e426294dd18f811a0041e2fa2dd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fe923860f72e8f57db4e614c3c2557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f43137725a6b4aa0d591",
+    "url": "/static/css/main.519aeec1.chunk.css"
+  },
+  {
+    "revision": "6c0e0dbd90a6be82c32f",
+    "url": "/static/js/2.252fb14f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.252fb14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e474e374b69d858c20",
+    "url": "/static/js/3.7419acb7.chunk.js"
+  },
+  {
+    "revision": "f43137725a6b4aa0d591",
+    "url": "/static/js/main.3f860d80.chunk.js"
+  },
+  {
+    "revision": "ba36db4fd8ea73347bc1",
+    "url": "/static/js/runtime-main.664bff1e.js"
+  }
+]);
